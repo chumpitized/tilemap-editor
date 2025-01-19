ba41362fc69046074964bbf96dd1a592ba31724d
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-01-19 11:05:41 -0600

    draw palettes and canvas and prepare to add sprites

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,53 @@
 #include <raylib.h>
 #include <iostream>
+#include <vector>
 
-int screenWidth = 1920;
+int screenWidth = 1295;
 int screenHeight = 1080;
 
-struct Vec2 {
+float tileSize		= 64;
+float canvasWidth	= 1024;
+float canvasHeight 	= 1024;
+float xOffset 		= 25;
+float yOffset 		= (screenHeight / 2) - (canvasHeight / 2);
+int canvasTileWidth = canvasWidth / tileSize;
+
+struct TileSqr {
 	int x;
 	int y;
+
+	int width;
+	int length;
+	std::vector<int> tiles;
+
+	TileSqr(int x, int y, int width, int length) {
+		this->x 	= x;
+		this->y 	= y;
+		this->width = width;
+		tiles 		= std::vector(0, length);
+	}
 };
 
-int main() {
-	float tileSize		= 64;
-	float canvasWidth	= 1024;
-	float canvasHeight 	= 1024;
-	float xOffset 		= 25;
-	float yOffset 		= (screenHeight / 2) - (canvasHeight / 2);
+void draw_tile_sqr(TileSqr tileSqr) {
+	for (int i = 0; i < tileSqr.width; ++i) {
+		for (int j = 0; j < tileSqr.width; ++j) {
+			float xTileOffset = tileSqr.x + (j * tileSize);
+			float yTileOffset = tileSqr.y + (i * tileSize);
 
-	int canvasTileWidth = canvasWidth / tileSize;
+			//We draw the sprite from the sprite array here... using the index from tiles...
+			Rectangle tile = Rectangle{xTileOffset, yTileOffset, tileSize, tileSize};		
+			
+			//this can just be black by default...
+			if (j + i & 1) DrawRectangleRec(tile, BLACK);
+			else DrawRectangleRec(tile, GRAY);
+		}
+	}
+}
+
+int main() {
+	TileSqr canvas 			= TileSqr(xOffset, yOffset, canvasTileWidth, canvasTileWidth * canvasTileWidth);
+	TileSqr entityPalette 	= TileSqr(xOffset + canvasWidth + 25, yOffset, 3, 9);
+	TileSqr tilePalette		= TileSqr(xOffset + canvasWidth + 25, yOffset + 300, 3, 7);
 
 	InitWindow(screenWidth, screenHeight, "Tilemap Editor");
 
@@ -24,18 +55,10 @@ int main() {
 		BeginDrawing();
 			ClearBackground(RAYWHITE);
 
-			//Draw the canvas as a grid of tiles
-			for (int i = 0; i < canvasTileWidth; ++i) {
-				for (int j = 0; j < canvasTileWidth; ++j) {			
-					float xTileOffset = xOffset + (j * tileSize);
-					float yTileOffset = yOffset + (i * tileSize);
-
-					Rectangle tile = Rectangle{xTileOffset, yTileOffset, tileSize, tileSize};		
+			draw_tile_sqr(canvas);
+			draw_tile_sqr(entityPalette);
+			draw_tile_sqr(tilePalette);
 			
-					if (j + i & 1) DrawRectangleRec(tile, BLACK);
-					else DrawRectangleRec(tile, GRAY);
-				}
-			}
 		EndDrawing();
 	}
 }
