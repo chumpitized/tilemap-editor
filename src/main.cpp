89256ae7afe5b3c733a395fec07ad9dea751bf8d
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-01-19 11:21:56 -0600

    don't assume sqr shape on draw

--- src/main.cpp
+++ src/main.cpp
@@ -21,26 +21,29 @@ struct TileSqr {
 	std::vector<int> tiles;
 
 	TileSqr(int x, int y, int width, int length) {
-		this->x 	= x;
-		this->y 	= y;
-		this->width = width;
-		tiles 		= std::vector(0, length);
+		this->x 		= x;
+		this->y 		= y;
+		this->width 	= width;
+		this->length 	= length;
+		tiles 			= std::vector(0, length);
 	}
 };
 
-void draw_tile_sqr(TileSqr tileSqr) {
-	for (int i = 0; i < tileSqr.width; ++i) {
-		for (int j = 0; j < tileSqr.width; ++j) {
-			float xTileOffset = tileSqr.x + (j * tileSize);
-			float yTileOffset = tileSqr.y + (i * tileSize);
+void draw_tile_sqr(TileSqr sqr) {
+	for (int i = 0; i < sqr.length; ++i) {
 
-			//We draw the sprite from the sprite array here... using the index from tiles...
-			Rectangle tile = Rectangle{xTileOffset, yTileOffset, tileSize, tileSize};		
+		int row = i / sqr.width;
+		int col = i - (row * sqr.width);
+
+		float xTileOffset = sqr.x + (col * tileSize);
+		float yTileOffset = sqr.y + (row * tileSize);
+
+		//We draw the sprite from the sprite array here... using the index from tiles...
+		Rectangle tile = Rectangle{xTileOffset, yTileOffset, tileSize, tileSize};		
 			
-			//this can just be black by default...
-			if (j + i & 1) DrawRectangleRec(tile, BLACK);
-			else DrawRectangleRec(tile, GRAY);
-		}
+		//this can just be black by default...
+		if (col + row & 1) DrawRectangleRec(tile, BLACK);
+		else DrawRectangleRec(tile, GRAY);
 	}
 }
 
