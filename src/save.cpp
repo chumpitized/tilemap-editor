5b840846ef1a9b56869d222b3f15cd018178c562
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-01-23 14:22:44 -0600

    save width and height and delimiter for puzzles

--- src/save.cpp
+++ src/save.cpp
@@ -8,8 +8,8 @@
 //draw error message when invalid...
 void save() {
 	std::vector<u16> trimmedCanvas;
-	int rowWidth 		= 0;
-	int expectedWidth 	= 0;
+	u8 rowWidth 		= 0;
+	u8 expectedWidth 	= 0;
 
 	for (auto val : canvas) {
 		if (val != 0xffff) {
@@ -28,20 +28,25 @@ void save() {
 		}
 	}
 
-	int height = (trimmedCanvas.size() / expectedWidth);
+	//save width and height as final u16
+	u8 height = (trimmedCanvas.size() / expectedWidth);
+	u16 widthAndHeight = expectedWidth << 8 | height;
+	trimmedCanvas.push_back(widthAndHeight);
 
 	std::fstream file;
     file.open("save/new_puzzle", std::ios::out | std::ios::binary);
  
 	//save puzzle to file
 	if (file) {
-		int length = trimmedCanvas.size() * 2;
+		int length = (trimmedCanvas.size() * 2) + 1;
 		uint8_t buffer[length];
 
 		for (int i = 0; i < trimmedCanvas.size(); i+=2) {
 			buffer[i] 		= trimmedCanvas[i] >> 8;
 			buffer[i + 1] 	= trimmedCanvas[i]; 
 		}
+		//delimiter in 1D array
+		buffer[length] = 0xff;
  
 		std::cout << "buffer length: " << length << std::endl;
 
@@ -50,6 +55,6 @@ void save() {
 	}
 
 	std::cout << "~~~SAVING~~~" << std::endl;
-	std::cout << "WIDTH: " << expectedWidth << std::endl;
-	std::cout << "HEIGHT: " << height << std::endl;
+	std::cout << "WIDTH: " << +expectedWidth << std::endl;
+	std::cout << "HEIGHT: " << +height << std::endl;
 }
